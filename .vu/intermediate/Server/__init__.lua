79731772f55859f8637a414c05dbec1f74d847a7
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2022-02-20 17:25:41 +0100

    add vua base files

---
+++ .vu/intermediate/Server/__init__.lua
@@ -0,0 +1,9 @@
+--[[
+    Venice Unleashed - Intermediate Lua binding
+    Type: __init__.lua
+    Generated on: Sun Feb 20 2022 17:25:24 GMT+0100 (Mitteleuropäische Normalzeit)
+--]]
+
+---@meta
+
+
